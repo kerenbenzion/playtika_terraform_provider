465127fc3a7bece69188cbe543a22065b494d4fa
kerenbenzion <48645213+kerenbenzion@users.noreply.github.com>
2020-12-29 09:22:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
         "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
         "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-        "github.com/rafi-barel/playtika-terraform-provider-drp/drpv4"
+        "github.com/kerenbenzion/playtika_terraform_provider/drp"
 )
 
 func main() {
         plugin.Serve(&plugin.ServeOpts{
                 ProviderFunc: func() *schema.Provider {
-                        return drpv4.Provider()
+                        return drp.Provider()
                 },
         })
 }
